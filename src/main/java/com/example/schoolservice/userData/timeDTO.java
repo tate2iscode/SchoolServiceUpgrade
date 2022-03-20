3bd43cee2c951e4ae54eecffae48a47f533943d3
kimdongyun <kimdongyun03@gmail.com>
2022-03-20 23:39:40 +0900

    데이결 서버 연결 끊김 해

---
+++ src/main/java/com/example/schoolservice/userData/timeDTO.java
@@ -0,0 +1,58 @@
+package com.example.schoolservice.userData;
+
+public class timeDTO {
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getMonth() {
+        return month;
+    }
+
+    public void setMonth(Integer month) {
+        this.month = month;
+    }
+
+    public Integer getDay() {
+        return day;
+    }
+
+    public void setDay(Integer day) {
+        this.day = day;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMin() {
+        return min;
+    }
+
+    public void setMin(Integer min) {
+        this.min = min;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public void setSecond(Integer second) {
+        this.second = second;
+    }
+
+    public Integer year = null;
+    public Integer month = null;
+    public Integer day = null;
+    public Integer hour = null;
+    public Integer min = null;
+    public Integer second = null;
+}
